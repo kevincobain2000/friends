d4e89d9ce58e04a2edd475e550bd3840246f69c0
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-11-03 03:34:50 +0900

    Tested the Fix for https://github.com/driftyco/ionic-contrib-frosted-glass/issues/7

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
